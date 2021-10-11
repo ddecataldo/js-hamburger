ba56216db19bc0bb31a51646a87db8d8ddbc8736
ddecataldo <davidedecataldo92@gmail.com>
2021-10-11 18:11:44 +0200

    Revisione esercizio (utilizzato classList.toggle)

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const hamburgerMenu = document.querySelector('.header-right > a');
+const hbMenu = document.querySelector('.hamburger-menu');
 
 hamburgerMenu.addEventListener("click", function(){
-    document.querySelector('.hamburger-menu').classList.add('active');
+    // document.querySelector('.hamburger-menu').classList.add('active');
+    hbMenu.classList.toggle('active');
 });
 
 const hamburgerMenuClose = document.querySelector('.hamburger-menu > a');
 
 hamburgerMenuClose.addEventListener("click", function(){
-    document.querySelector('.hamburger-menu').classList.remove('active');
+    // document.querySelector('.hamburger-menu').classList.remove('active');
+    hbMenu.classList.toggle('active');
 });
-
